927db45ab356f9f50c1f4e700a99199054478b17
Kumarchidand <Kchidanandam000@gmail.com>
2023-08-04 23:55:40 +0530

    3rd commit

---
+++ music_lists.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "Desi kalakar",
+        artist: "Honey singh",
+        img: "music1",
+        src: "music1"
+    },
+    {
+        name: "Mi Amor",
+        artist: "Unknown",
+        img: "music2",
+        src: "music2"
+    },
+    {
+        name: "Why This Kolaveri Di",
+        artist: "Dhanush",
+        img: "music3",
+        src: "music3"
+    },
+    {
+        name: "Arey Pagal Hoye Jabo",
+        artist: "Unknown",
+        img: "music4",
+        src: "music4"
+    },
+    {
+        name: " Habibi 8D",
+        artist: "Unknown",
+        img: "music5",
+        src: "music5"
+    },
+]
